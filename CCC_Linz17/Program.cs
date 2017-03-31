830aa363f2ddddae8b18164a6a4da579ea0a6572
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2017-03-31 13:44:33 +0200

    Add spatial library

--- CCC_Linz17/Program.cs
+++ CCC_Linz17/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathNet.Spatial.Euclidean;
 using Newtonsoft.Json;
 
 namespace CCC_Linz17
